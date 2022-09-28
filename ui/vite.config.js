ed8fd4c2328aba56b7db1fb6b0e22f8bfbe12a22
terrygonguet <terry@gonguet.com>
2022-09-29 00:35:38 +0200

    build(ui): :heavy_plus_sign: Add unplugin icons and svelte-syncable packages

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
+import Icons from "unplugin-icons/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svelte({ emitCss: false })],
+	plugins: [svelte({ emitCss: false }), Icons({ compiler: "svelte" })],
 	build: {
 		lib: {
 			entry: "src/main.js",
