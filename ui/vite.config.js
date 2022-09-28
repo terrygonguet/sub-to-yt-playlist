d43b6c695caea07df3dbb0e7d764c58065b5a2e0
terrygonguet <terry@gonguet.com>
2022-09-28 18:16:12 +0200

    build(ui): :building_construction: Better npm scripts & vite config

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -3,11 +3,14 @@ import { svelte } from "@sveltejs/vite-plugin-svelte"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svelte()],
+	plugins: [svelte({ emitCss: false })],
 	build: {
 		lib: {
 			entry: "src/main.js",
 			formats: ["es"],
 		},
+	},
+	server: {
+		hmr: false,
 	},
 })
